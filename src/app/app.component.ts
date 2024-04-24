55e059472c656e42eeab62ad1b044a49a60f47ab
Bob Glass <glassrmux@gmail.com>
2024-04-24 08:40:30 -0500

    Add ag-grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { ColDef } from 'ag-grid-community';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-quartz.css'
+import { AgGridAngular } from 'ag-grid-angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [AgGridAngular],
+  template: 
+  `
+  <ag-grid-angular
+    class="ag-theme-quartz" style="height: 500px"
+    [rowData]="rowData" [columnDefs]="colDefs"/>
+  `
 })
+
 export class AppComponent {
-  title = 'my-ag-grid-app';
+  rowData = [
+    { make: "Tesla", model: "Model Y", price: 64950, electiric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electiric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electiric: false },
+  ];
+
+  colDefs: ColDef[] = [
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+    { field: "electiric" },
+  ]
 }
